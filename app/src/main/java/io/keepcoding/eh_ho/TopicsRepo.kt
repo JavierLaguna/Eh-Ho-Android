a3e13465d25190eee08bd9ccff3692e6534da5da
Javier Laguna <javier.laguna92@gmail.com>
2020-07-22 00:39:50 +0200

    Day 2

---
+++ app/src/main/java/io/keepcoding/eh_ho/TopicsRepo.kt
@@ -0,0 +1,27 @@
+package io.keepcoding.eh_ho
+
+// Singleton
+object TopicsRepo {
+    val topics: MutableList<Topic> = mutableListOf()
+        get() {
+            if (field.isEmpty()) {
+                field.addAll(getDummyTopics())
+            }
+            return field
+        }
+
+//    fun getDummyTopics() {
+//        for (i in 0..10) {
+//            val topic = Topic(title = "Topic $i", content = "Content $i")
+//            this.topics.add(topic)
+//        }
+//    }
+
+//    fun getDummyTopics(count: Int = 10): List<Topic> {
+//        return (0..count).map { Topic(title = "Topic $it", content = "Content $it") }
+//    }
+
+    fun getDummyTopics(count: Int = 10) =
+        (0..count).map { Topic(title = "Topic $it", content = "Content $it") }
+
+}
