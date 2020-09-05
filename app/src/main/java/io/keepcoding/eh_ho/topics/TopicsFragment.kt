1d0026032fc8aa73795b9f3e250be8ac7d71eecf
Javier Laguna <javier.laguna92@gmail.com>
2020-09-05 13:37:11 +0200

    Add CoordinatorLayout with layout behavior for animate floating button when scroll topics

--- app/src/main/java/io/keepcoding/eh_ho/topics/TopicsFragment.kt
+++ app/src/main/java/io/keepcoding/eh_ho/topics/TopicsFragment.kt
@@ -2,10 +2,14 @@ package io.keepcoding.eh_ho.topics
 
 import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.*
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.keepcoding.eh_ho.R
 import io.keepcoding.eh_ho.data.Topic
 import io.keepcoding.eh_ho.data.TopicsRepo
@@ -140,5 +144,59 @@ class TopicsFragment : Fragment() {
         fun onCreateTopic()
         fun onShowPosts(topic: Topic)
         fun onLogout()
+    }
+}
+
+class ScrollAwareFABBehavior(context: Context?, attrs: AttributeSet?) :
+    FloatingActionButton.Behavior(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(
+            coordinatorLayout,
+            child,
+            directTargetChild,
+            target,
+            axes,
+            type
+        )
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+
+        if (dyConsumed > 0) {
+            val params = child.layoutParams as CoordinatorLayout.LayoutParams
+            val toTranslate = child.height.toFloat() + params.bottomMargin
+            child.animate().translationY(toTranslate)
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0F)
+        }
     }
 }
